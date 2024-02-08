35b375a7af29057deb3a32190b6d8f8a36758c10
Ayowale <haydexwisdom@gmail.com>
2024-02-08 15:52:02 +0000

    Fixed

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Encrypt module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using a random salt
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks a hashed password against the provided password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
