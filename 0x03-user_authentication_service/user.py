8396fdf4e0e528ed5e50653923cf36b65c7e8b47
Ayowale <haydexwisdom@gmail.com>
2024-02-22 13:43:02 +0000

    Task 0 fixed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,15 +5,19 @@ SQLAlchemy model named <User>
 """
 
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class User:
+Base = declarative_base()
+
+
+class User(Base):
     """
     model for users database table
     """
     __tablename__ = "users"
-    id = Column(Integer)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
